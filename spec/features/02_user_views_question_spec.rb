9530f1b373aca149c2b63b3844d3885fed19b454
Rachel Adler <adler.rachel.a@gmail.com>
2016-06-14 12:06:55 -0400

    tests changed slightly

--- spec/features/02_user_views_question_spec.rb
+++ spec/features/02_user_views_question_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 feature "User views question page" do
-  let!(:q4) { Question.create(title: "#{Faker::Hipster.sentence(10)}", description: "#{Faker::Hipster.paragraph(6, true)}") }
+  let!(:q1) { Question.create(title: "#{Faker::Hipster.sentence(10)}", description: "#{Faker::Hipster.paragraph(6, true)}") }
 
   scenario "clicks on a question title from main page and sees question page" do
     visit '/questions'
-    click_on "#{q4.title}"
-    expect(page).to have_current_path(question_path(q4))
+    click_on "#{q1.title}"
+    expect(page).to have_current_path(question_path(q1))
   end
 
   scenario "views questions list" do
-    visit question_path(q4)
-    expect(page).to have_content "#{q4.title}"
-    expect(page).to have_content "#{q4.description}"
+    visit question_path(q1)
+    expect(page).to have_content "#{q1.title}"
+    expect(page).to have_content "#{q1.description}"
   end
 
 end
