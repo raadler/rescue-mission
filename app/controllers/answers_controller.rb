0902848de0d5be6aa7a876d997bd08ffca4ecbf3
Rachel Adler <adler.rachel.a@gmail.com>
2016-06-14 17:08:53 -0400

    fixed the answer create method

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,10 +2,11 @@ class AnswersController < ApplicationController
 
   def create
     @answer = Answer.new(answer_params)
+    @question = Question.find(params[:question_id])
+    @answer.question = @question
     if @answer.save
       flash[:success] = "Answer successfully submitted!"
-      @question = @answer.question
-      redirect to question_path(@question)
+      redirect_to question_path(@question)
     else
       flash[:errors] = @answer.errors.full_messages.join(', ')
       render :"questions/show"
@@ -16,8 +17,7 @@ class AnswersController < ApplicationController
 
   def answer_params
     params.require(:answer).permit(
-      :description,
-      :question
+      :description
     )
   end
 end
--- spec/features/04_user_posts_answer_spec.rb
+++ spec/features/04_user_posts_answer_spec.rb
@@ -5,7 +5,6 @@ feature "User posts an answer" do
 
   scenario "User successfully answers a question" do
     visit question_path(q1)
-    binding.pry
 
     fill_in "Answer", with: "Stop letting the cats run across the keyboard; put it away as soon as you see a cat."
     click_button "Submit Answer"
